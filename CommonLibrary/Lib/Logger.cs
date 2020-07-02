34a2539a7f7b9543c21b47a16a9180ab964904b2
takmg <takmg@norepry.github.com>
2020-07-03 00:40:20 +0900

    初回コミット

---
+++ CommonLibrary/Lib/Logger.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace CommonLibrary.Lib
+{
+    /// <summary>
+    /// ログ出力用クラス
+    /// </summary>
+    public class Logger
+    {
+        /// <summary>
+        /// 現在のタイムスタンプ
+        /// </summary>
+        public static readonly string Timestamp = DateTime.Now.ToString("yyyyMMddHHmmss");
+
+        /// <summary>
+        /// 出力先ファイル名
+        /// </summary>
+        private string FileName { get; set; }
+
+        /// <summary>
+        /// コンソール出力モード
+        /// </summary>
+        private bool ConsoleOutputFlag { get; set; }
+
+        /// <summary>
+        /// 出力パス
+        /// </summary>
+        public string ExportPath { get; set; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="filename"></param>
+        public Logger(string filename = null, bool consoleOutputFlag = true)
+        {
+            // ファイル名が存在しない場合はファイル名を作成する。
+            if (string.IsNullOrEmpty(filename))
+            {
+                filename = $"{Timestamp}-log.log";
+            }
+
+            // ファイル名の指定
+            FileName = filename;
+
+            // コンソール出力フラグ
+            ConsoleOutputFlag = consoleOutputFlag;
+        }
+
+        /// <summary>
+        /// 書き込みを行う
+        /// </summary>
+        /// <param name="sa"></param>
+        public void WriteLine(string value)
+        {
+            // コンソールログ出力を実施
+            if (ConsoleOutputFlag) { Console.WriteLine(value); }
+
+            // 文字コードを指定
+            Encoding enc = Encoding.GetEncoding("UTF-8");
+
+            // StreamWriterを実行
+            string loggerPath = $"{ExportPath}/log";
+            Directory.CreateDirectory(loggerPath);
+            using (StreamWriter writer = new StreamWriter($"{loggerPath}/{FileName}", true, enc))
+            {
+                writer.WriteLine(value);
+            }
+        }
+
+        /// <summary>
+        /// セパレータの描画
+        /// </summary>
+        public void WriteSeparator()
+        {
+            WriteLine("---------------------------------------------------------------------");
+        }
+    }
+}
---
+++ CommonLibrary/Lib/Yaml.cs
@@ -0,0 +1,82 @@
+﻿using System.IO;
+using System.Text;
+using YamlDotNet.RepresentationModel;
+
+namespace CommonLibrary.Lib
+{
+    public class Yaml
+    {
+        /// <summary>
+        /// YAMLファイル名
+        /// </summary>
+        public string SettingFileName { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string RootNodeName { get; set; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public Yaml(string settingFilename, string rootNodeName)
+        {
+            SettingFileName = settingFilename;
+            RootNodeName = rootNodeName;
+        }
+
+        /// <summary>
+        /// 設定ファイルを取得
+        /// </summary>
+        public string ReadString(string key)
+        {
+            try
+            {
+                using (var input = new StreamReader(SettingFileName, Encoding.UTF8))
+                {
+                    // YAMLをロード
+                    var yaml = new YamlStream();
+                    yaml.Load(input);
+
+                    // 
+                    var mapping = (YamlMappingNode)yaml.Documents[0].RootNode[RootNodeName];
+                    return ((YamlScalarNode)mapping.Children[new YamlScalarNode(key)])?.Value;
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public bool? ReadBool(string key)
+        {
+            try
+            {
+                return bool.Parse(ReadString(key));
+            }
+            catch { }
+            return null;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public int? ReadInt(string key)
+        {
+            try
+            {
+                return int.Parse(ReadString(key));
+            }
+            catch { }
+            return null;
+        }
+    }
+}
